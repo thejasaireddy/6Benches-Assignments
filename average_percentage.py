4bc8dfd8807ab82ca67ff2e7472f7d7b256155fc
Theja Kunuthuru <theja.kunuthuru@gmail.com>
2020-03-10 16:15:31 +0530

    average_percentage evaluation file added

--- average_percentage.py
+++ average_percentage.py
@@ -4,7 +4,7 @@ for m in range(5):
     marks_list.append(marks)
 
 average = sum(marks_list)/5
-percentage = sum(marks_list)/50*100
+percentage = sum(marks_list)/50*100 #assuming each subject is for 10 marks
 
 print("Average marks: "+str(average))
 print("Percentage marks: "+str(percentage))

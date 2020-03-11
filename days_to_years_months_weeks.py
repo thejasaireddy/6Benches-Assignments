bbfa4bfd578ca2794d55fc7eaf5649f5382ec0cf
Theja Kunuthuru <theja.kunuthuru@gmail.com>
2020-03-11 11:16:03 +0530

    years, months, weeks evaluation file added

---
+++ days_to_years_months_weeks.py
@@ -0,0 +1,9 @@
+days = int(input("Enter the number of days: "))
+
+years = days/365
+
+months = days/30
+
+weeks = days/7
+
+print("Total year, months, weeks for given days are " + str(years) + " " + str(months) + " " + str(weeks) + "respectively")
